3d1d3b90b70207c09e5822a92e7e60d95feeeaa0
DJ <rivers.creeks@gmail.com>
2018-06-30 16:42:37 -0700

    end lesson 15 id to detail template

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -2,9 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return {
-      id: `author-${params.id}`
-    };
-
+    return this.store.findRecord('author', params.id);
   }
 });
