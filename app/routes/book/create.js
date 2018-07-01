b79d76625f3603bbdf4dd9f44cee3a665143b981
DJ <rivers.creeks@gmail.com>
2018-07-01 16:00:31 -0700

    end lesson 31 completed book create form

--- app/routes/book/create.js
+++ app/routes/book/create.js
@@ -3,7 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
-    return this.store.findAll('author');
+    return {
+      title: '',
+      isbn: '',
+      publishedDate: null,
+      author: null
+    };
   }
 
 });
