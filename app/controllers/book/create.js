0352e1579e0d255eee59a31c0e32f00515be13d7
DJ <rivers.creeks@gmail.com>
2018-07-01 15:32:19 -0700

    end lesson 30 implemented power-select search for authors in book create

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -5,6 +5,11 @@ export default Controller.extend({
   actions: {
     selectAuthor(author) {
       console.log(author);
+    },
+
+    searchAuthor(query) {
+      return this.store.query('author', { filter: {query} });
+
     }
   }
 
