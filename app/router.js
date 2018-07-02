cf618b2b5d65920ac16b1a9b5c57a0fbbc512e6d
DJ <rivers.creeks@gmail.com>
2018-07-01 17:16:17 -0700

    working on books CRUD

--- app/router.js
+++ app/router.js
@@ -12,7 +12,8 @@ Router.map(function() {
     this.route('create');
     this.route('edit', { path: '/:id/edit' });
   });
-  this.route('book', function() {
+  this.route('book', { path: '/books'}, function() {
+    this.route('detail', { path: '/:id'});
     this.route('create');
   });
 });
