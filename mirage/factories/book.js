6bf98c3fcf26807f80f9fe3881ade582660d8bd3
Don Denton <don@happycollision.com>
2018-07-07 17:38:52 -0500

    better titles and isbns

--- mirage/factories/book.js
+++ mirage/factories/book.js
@@ -3,7 +3,7 @@ import { Factory, faker, association } from 'ember-cli-mirage';
 export default Factory.extend({
   author: association(),
 
-  title() { return faker.hacker.verb() },
-  isbn() { return '12345' },
+  title() { return faker.fake('{{hacker.ingverb}} any {{hacker.adjective}} {{hacker.noun}}') },
+  isbn() { return faker.fake('{{random.number}}-{{random.number}}-{{random.number}}') },
   publishedDate() { return faker.date.past() },
 });
